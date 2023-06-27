4ce06c51dd08479ac66af96847965f878e4e5e05
Cqollo <cokatch@yahoo.com>
2023-06-27 20:46:53 +0300

    Added a few buttons for the restaurant app

--- React by META/src/useReducer.js
+++ React by META/src/useReducer.js
@@ -3,12 +3,12 @@ import { useReducer } from "react";
 
 const reducer = (state, action) => {
   if (action.type === "buy_ingredients") return { money: state.money - 200 };
-  if (action.type === "sell_a_meal") return { money: state.money + 10 };
-  if (action.type === "sell_a_meal") return { money: state.money + 10 };
-  if (action.type === "sell_a_meal") return { money: state.money + 10 };
-  if (action.type === "sell_a_meal") return { money: state.money + 10 };
-  if (action.type === "sell_a_meal") return { money: state.money + 10 };
-  if (action.type === "sell_a_meal") return { money: state.money + 10 };
+  if (action.type === "sell_a_doughnut") return { money: state.money + 5 };
+  if (action.type === "sell_a_pancake") return { money: state.money + 10 };
+  if (action.type === "sell_a_meatroll") return { money: state.money + 20 };
+  if (action.type === "sell_a_glasswine") return { money: state.money + 40 };
+  if (action.type === "sell_a_chapati") return { money: state.money + 12 };
+  if (action.type === "sell_a_coke") return { money: state.money + 2 };
   if (action.type === "Host_a_celebrity") return { money: state.money + 5000 };
   return state;
 };
@@ -24,16 +24,22 @@ function UpdateIncome() {
         <button onClick={() => dispatch({ type: "buy_ingredients" })}>
           Shopping for veggies
         </button>
-        <button onClick={() => dispatch({ type: "sell_a_meal" })}>
-          Serve a meal to the customers
+        <button onClick={() => dispatch({ type: "sell_a_doughnut" })}>
+          Serve a snack to the customers
         </button>
-        <button onClick={() => dispatch({ type: "Host_a_celebrity" })}>
+        <button onClick={() => dispatch({ type: "sell_a_pancake" })}>
           Host a celeb
         </button>
-        <button onClick={() => dispatch({ type: "Host_a_celebrity" })}>
+        <button onClick={() => dispatch({ type: "sell_a_meatroll" })}>
           Host a celeb
         </button>
-        <button onClick={() => dispatch({ type: "Host_a_celebrity" })}>
+        <button onClick={() => dispatch({ type: "sell_a_glasswine" })}>
+          Host a celeb
+        </button>
+        <button onClick={() => dispatch({ type: "sell_a_chapati" })}>
+          Host a celeb
+        </button>
+        <button onClick={() => dispatch({ type: "sell_a_coke" })}>
           Host a celeb
         </button>
         <button onClick={() => dispatch({ type: "Host_a_celebrity" })}>
